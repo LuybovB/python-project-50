3cb1f6a9a2ff1512378285aa99789d8be0784bd6
LuybovB <grandprinceess7777@gmail.com>
2023-12-13 21:38:32 +0300

    2step

--- gendiff/scripts/gendiff.py
+++ gendiff/scripts/gendiff.py
@@ -7,7 +7,7 @@ def main():
     # add other arguments
     parser.add_argument('first_file')
     parser.add_argument('second_file')
-
+    parser.add_argument('-f', '--format', default='plain', help='set format of output')
     args = parser.parse_args()
 
 
