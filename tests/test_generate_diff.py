7843d61cd7a162603e321a45a4a63a50186735da
LuybovB <grandprinceess7777@gmail.com>
2024-01-26 23:17:58 +0300

    5step

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,14 +1,15 @@
 from gendiff.diff_with_formatter import generate_diff
 import os
 
+
 file1_path = os.path.join(os.path.dirname(__file__), "fixtures", "file1.json")
 file2_path = os.path.join(os.path.dirname(__file__), "fixtures", "file2.json")
-file11_path = os.path.join(os.path.dirname(__file__), "fixtures", "file1.yml")
-file22_path = os.path.join(os.path.dirname(__file__), "fixtures", "file2.json")
+file11_path = os.path.join(os.path.dirname(__file__), "fixtures", "file11.yml")
+file22_path = os.path.join(os.path.dirname(__file__), "fixtures", "file22.yml")
 
 test_data = [
     (file1_path, file2_path, "fixtures/file3.json"),
-    (file1_path, file2_path, "fixtures/file3.yml"),
+    (file11_path, file22_path, "fixtures/file33.yml"),
 ]
 
 
